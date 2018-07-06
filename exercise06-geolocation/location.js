83e2988f640bdb1aa24c51b13851f90638d40d3b
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2018-07-06 11:19:08 +0300

    Moved exercise 6 code inside a module

--- exercise06-geolocation/location.js
+++ exercise06-geolocation/location.js
@@ -5,75 +5,80 @@
  */
 
 /*global google*/
-/* exported initAll */
-
-var latLong = {
-    lat: 60.0,
-    lng: 20.0
-};
+/* exported geoModule */
 
 /**
- * Locates the user and outputs location information
- * @param {Function} callback that handles what happens with successful retrieval of location
+ * Locates the user and outputs the location to google map in drawn inside the document
+ * @module geoModule
+ * 
+ * @returns {Object} has method 'initAll' that executes the module functionality
  */
-function geoFindMe(callback) {
-    var output = document.getElementById('out');
+var geoModule = (function() {
+    var latLong = {
+        lat: 60.0,
+        lng: 20.0
+    };
+    
+    /**
+     * Locates the user and outputs location information
+     * @param {Function} callback that handles what happens with successful retrieval of location
+     */
+    function geoFindMe(callback) {
+        var output = document.getElementById('out');
 
-    if (!navigator.geolocation) {
-        output.innerHTML = '<p>Geolocation is not supported by your browser</p>';
-        return;
-    }
-
-    function success(position) {
-        latLong.lat = position.coords.latitude;
-        latLong.lng = position.coords.longitude;
-
-        output.innerHTML = '<p>Latitude is ' + latLong.lat + '° <br>Longitude is ' + latLong.lng + '°</p>';
-        
-        if (typeof callback === 'function') {
-            callback(latLong);
-        } else {
-            console.log('callback not a function');
+        if (!navigator.geolocation) {
+            output.innerHTML = '<p>Geolocation is not supported by your browser</p>';
+            return;
         }
+    
+        function success(position) {
+            latLong.lat = position.coords.latitude;
+            latLong.lng = position.coords.longitude;
+    
+            output.innerHTML = '<p>Latitude is ' + latLong.lat + '° <br>Longitude is ' + latLong.lng + '°</p>';
+            
+            if (typeof callback === 'function') {
+                callback(latLong);
+            } else {
+                console.log('callback not a function');
+            }
+        }
+    
+        function error() {
+            output.innerHTML = 'Unable to retrieve your location';
+        }
+    
+        output.innerHTML = '<p>Locating…</p>';
+        navigator.geolocation.getCurrentPosition(success, error);
     }
-
-    function error() {
-        output.innerHTML = 'Unable to retrieve your location';
+    
+    /**
+     * Draws a gogle map inside browser window
+     * @param {Object} Object with latitude and longitude properties
+     */
+    function initMap(position) {
+        var location = position;
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 12,
+            center: location
+        });
+    
+        var infowindow = new google.maps.InfoWindow({
+            content: 'My Location'
+        });
+    
+        var marker = new google.maps.Marker({
+            position: location,
+            map: map,
+            label: ':)'
+        });
+    
+        infowindow.open(map, marker);
     }
-
-    output.innerHTML = '<p>Locating…</p>';
-
-    navigator.geolocation.getCurrentPosition(success, error);
-}
-
-/**
- * Draws a gogle map inside browser window
- * @param {Object} Object with latitude and longitude properties
- */
-function initMap(position) {
-    var location = position;
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 12,
-        center: location
-    });
-
-    var infowindow = new google.maps.InfoWindow({
-        content: 'My Location'
-    });
-
-    var marker = new google.maps.Marker({
-        position: location,
-        map: map,
-        label: ':)'
-    });
-
-    infowindow.open(map, marker);
-}
-
-/**
- * Call geoFindMe-function and pass in a callback function which
- * geoFindMe-function runs when it has located the user.
- */
-function initAll() {
-    geoFindMe(initMap);
-}
+    
+    return {
+        initAll: function () {
+            geoFindMe(initMap);
+        }
+    };
+}());
