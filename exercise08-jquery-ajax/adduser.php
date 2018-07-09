6057c58a6322a44c8634f2057db775aa7f254a99
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2018-07-09 15:06:27 +0300

    Created exercise08. Copied initial exercise material to folder.

---
+++ exercise08-jquery-ajax/adduser.php
@@ -0,0 +1,31 @@
+﻿<?php
+
+/*
+  adduser.php
+  Skripti jolla lisätään käyttäjät kantaan
+  mikäli ajax.js -skriptin enableButton()
+  -funktiossa se sallitaan
+ */
+
+//Poimitaan syötetyt arvot lomakkeelta
+//sisääntulevan datan filtteröintiä ei ole
+$username = $_POST['username'];
+$password = $_POST['password'];
+$submit = $_POST['submit'];
+
+//jos nappia on painettu
+if ($submit) {
+
+    //yhteydenotto kantaan kannattaisi laittaa connect.php -tiedostoon jonka voisi incluudata
+    //joka sivulle jossa sitä tarvitaan
+    $yhteys = mysqli_connect("localhost", "root", "") or die("Yhteyttä kantaan ei saatu");
+    mysqli_select_db($yhteys, "websk1_esim") or die("Tietokantaa ei löydy");
+
+    $query = "INSERT INTO user SET username='$username', password='$password'";
+
+    mysqli_query($yhteys, $query) or die("Tietojen lisääminen ei onnistunut<br>"
+                    . mysqli_error());
+
+
+    echo "Käyttäjä lisätty!<meta http-equiv=refresh content='0; url=index.html'>";
+}
---
+++ exercise08-jquery-ajax/dbquery.php
@@ -0,0 +1,42 @@
+﻿<?php
+/* dbquery.php
+  Tällä PHP-skriptillä otetaan yhteys MySQL-tietokantaan ja haetaan tieto
+  siitä onko lomakkeelle syötetty tunnus kannassa. Tämä skripti ei toimi suoraan HTML-sivun
+  kanssa kuten perinteisissä sovelluksissa vaan PHP-skriptin ja HTML-sivun välissä toimii ajax.js
+
+  Testausta varten tarvitset seuraavanlaisen taulun jonka voit laittaa esim. kantaan nimeltään websk:
+
+  CREATE TABLE `user` (
+  `username` varchar(20) NOT NULL default '',
+  `password` varchar(20) NOT NULL default '',
+  `ID` int(11) NOT NULL auto_increment,
+  PRIMARY KEY  (`ID`)
+  ) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=0 ;
+ */
+
+//poimitaan ajax.js -skriptistä input-kenttään syötetty arvo
+//sisääntulevan datan filtteröintiä ei ole
+$value = $_GET['q'];
+
+//yhteydenotto kantaan kannattaisi laittaa connect.php -tiedostoon jonka voisi incluudata
+//joka sivulle jossa sitä tarvitaan
+$yhteys = mysqli_connect("localhost", "root", "") or die("Yhteyttä kantaan ei saatu");
+mysqli_select_db($yhteys, "websk1_esim") or die("Tietokantaa ei löydy");
+
+//valitaan kaikki tietueet joiden username-kentässä on lomakkeelle syötetty arvo
+$query = "SELECT * FROM user where username ='$value'";
+$result = mysqli_query($yhteys, $query);
+//niiden rivien määrä jotka toteuttavavat hakuehdon 
+$num_rows = mysqli_num_rows($result);
+
+//jos hakuehdon toteuttavia rivejä ei ole, on tunnus käytettävissä
+if ($num_rows == 0) {
+    $msg = 'Käytettävissä';
+} else {
+    $msg = 'On jo käytössä, valitse joku muu!';
+}
+
+/* Lopuksi tulostetaan viesti joka lähtee ajax.js -skriptille tekstimuodossa.
+  Muotona voisi olla myös esim. JSON tai XML jos lähetettäisiin paljon dataa
+  jolla on tietty rakennne */
+echo $msg;
