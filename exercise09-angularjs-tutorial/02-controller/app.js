5a20328e88b746a219e264085edf8681ec96ab05
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2018-07-12 10:11:04 +0300

    Renamed contactController file. Changed syntax style.

---
+++ exercise09-angularjs-tutorial/02-controller/app.js
@@ -0,0 +1,14 @@
+/* globals angular */
+
+// Define the module
+var contactApp = angular.module('contactApp', []);
+
+// Define controller for the module
+contactApp.controller('ContactController', function($scope) {
+    $scope.contacts = ['hi@email.com', 'hello@email.com'];
+    
+    $scope.add = function() {
+        $scope.contacts.push($scope.newcontact);
+        $scope.newcontact = '';
+    };
+});
