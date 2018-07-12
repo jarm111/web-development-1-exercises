38301b3ed1626ef07049aaff5a869c06fc49f1f9
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2018-07-12 10:16:28 +0300

    Named function

--- exercise09-angularjs-tutorial/02-controller/app.js
+++ exercise09-angularjs-tutorial/02-controller/app.js
@@ -4,7 +4,7 @@
 var contactApp = angular.module('contactApp', []);
 
 // Define controller for the module
-contactApp.controller('ContactController', function($scope) {
+contactApp.controller('ContactController', function ContactController($scope) {
     $scope.contacts = ['hi@email.com', 'hello@email.com'];
     
     $scope.add = function() {
