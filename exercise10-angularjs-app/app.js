73e876ed523765ebfc01b2801bd42bb232b103f0
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2018-07-18 13:36:24 +0300

    Created template routing

---
+++ exercise10-angularjs-app/app.js
@@ -0,0 +1,35 @@
+/* globals angular */
+
+/**
+ * RegistrationApp Module
+ */
+var registrationApp = angular.module('registrationApp', ['ngRoute']);
+
+/**
+ * Routing
+ */
+registrationApp.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/ShowRegistrations', {
+        templateUrl: 'templates/show_registrations.html',
+        controller: 'ShowRegistrationsController'
+    }).when('/Register', {
+        templateUrl: 'templates/register.html',
+        controller: 'RegisterController'
+    }).otherwise({
+        redirectTo: '/Register'
+    });
+}]);
+
+/**
+ * Controller that handles showing of registrations
+ */
+registrationApp.controller('ShowRegistrationsController', ['$scope', function($scope) {
+    $scope.message = 'This is show_registrations screen';
+}]);
+
+/**
+ * Controller that handles Register page
+ */
+registrationApp.controller('RegisterController', ['$scope', function($scope) {
+    $scope.message = 'This is register screen';
+}]);
