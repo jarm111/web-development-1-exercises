cf5efd5d2b758aaacbacace7aa4974c6879ebfbb
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2018-08-01 11:27:30 +0300

    Created AnswerOption and Result components

--- exercise14-quiz-game-react/src/App.js
+++ exercise14-quiz-game-react/src/App.js
@@ -1,36 +1,50 @@
 import React, { Component } from 'react';
 import './App.css';
 
+function AnswerOption(props) {
+    return (
+        <div>
+            <input type="radio" name="option" value={props.value} />{props.value}<br />
+        </div>
+    );
+}
+
+function Result(props) {
+    return (
+        <p>Answer: {props.answer}</p>
+    )
+}
+
 function Question(props) {
-  return (
-    <div>
-      <p>Which disease devastated livestock across the UK during 2001?</p>
-      <form>
-        <input type="radio" name="option" value="" />Hand-and-foot<br />
-        <input type="radio" name="option" value="" />Foot-in-mouth<br />
-        <input type="radio" name="option" value="" />Hand-to-mouth<br />
-        <input type="radio" name="option" value="" />Foot-and-mouth<br />
-        <input type="button" onclick="" defaultValue="Answer" />
-      </form>
-      <p>Answer: Foot-and-mouth</p>
-    </div>
-  );
+    return (
+        <div>
+            <p>Which disease devastated livestock across the UK during 2001?</p>
+            <form>
+                <AnswerOption value="Hand-and-foot" />
+                <AnswerOption value="Foot-in-mouth" />
+                <AnswerOption value="Hand-to-mouth" />
+                <AnswerOption value="Foot-and-mouth" />
+                <input type="button" onClick="" defaultValue="Answer" />
+            </form>
+            <Result answer="Foot-and-mouth" />
+        </div>
+    );
 }
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Quiz Game with React</h1>
-        </header>
-        <p className="App-intro">
-          Question #1
-        </p>
-        <Question />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1 className="App-title">Quiz Game with React</h1>
+                </header>
+                <p className="App-intro">
+                    Question #1
+                </p>
+                <Question />
+            </div>
+        );
+    }
 }
 
 export default App;
