788d183e8b6586367648446a5ecaa84004fe173c
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2018-08-01 11:00:48 +0300

    Hardcoded quiz form

--- exercise14-quiz-game-react/src/App.js
+++ exercise14-quiz-game-react/src/App.js
@@ -1,6 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
 
+function Question(props) {
+  return (
+    <div>
+      <p>Which disease devastated livestock across the UK during 2001?</p>
+      <form>
+        <input type="radio" name="option" value="" />Hand-and-foot<br />
+        <input type="radio" name="option" value="" />Foot-in-mouth<br />
+        <input type="radio" name="option" value="" />Hand-to-mouth<br />
+        <input type="radio" name="option" value="" />Foot-and-mouth<br />
+        <input type="button" onclick="" defaultValue="Answer" />
+      </form>
+      <p>Answer: Foot-and-mouth</p>
+    </div>
+  );
+}
+
 class App extends Component {
   render() {
     return (
@@ -9,8 +25,9 @@ class App extends Component {
           <h1 className="App-title">Quiz Game with React</h1>
         </header>
         <p className="App-intro">
-          asd
+          Question #1
         </p>
+        <Question />
       </div>
     );
   }
