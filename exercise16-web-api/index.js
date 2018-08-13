5e0e94794e7e9b8585b43071ad4ca9b85f1be569
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2018-08-13 14:35:01 +0300

    Formatting for table data

--- exercise16-web-api/index.js
+++ exercise16-web-api/index.js
@@ -1,28 +1,48 @@
-
 $(document).ready(function () {
     $.getJSON('https://www.googleapis.com/books/v1/volumes?q=javascript', function (data) {
         for (var i = 0; i < data.items.length; i++) {
-            // var item = data.items[i];
-            // $('#content').append('<br>' + item.volumeInfo.title);
             var vol = data.items[i].volumeInfo;
-            appendBooks(i, vol.title, vol.authors[0], vol.publishedDate, vol.description);
+            appendBooks(i, vol.title, vol.subtitle, vol.authors, vol.publishedDate, vol.description);
         }
         console.log(data);
     });
 });
 
-function appendBooks(tableRowIndex, title, author, date, description) {
+function appendBooks(tableRowIndex, title, subTitle, authors, date, description) {
     $('#books').append(
         '<tr><th scope="row">'
         + ++tableRowIndex +
         '</th><td>'
-         + title +
+         + formatTitle(title, subTitle) +
          '</td><td>'
-         + author +
+         + formatAuthors(authors) +
          '</td><td>'
-         + date +
+         + getYearFromDate(date) +
          '</td><td>'
-         + description +
+         + formatDescription(description, 200) +
          '</td></tr>'
     );
+}
+
+function formatTitle(title, subtitle) {
+    if (subtitle) {
+        return title + ' - ' + subtitle;
+    }
+    
+    return title;
+}
+
+function formatAuthors(authors) {
+    return authors.join(', ');
+}
+
+function getYearFromDate(date) {
+    return new Date(date).getFullYear();
+}
+
+function formatDescription(description, maxLength) {
+    if (description.length > maxLength) {  
+        return description.substring(0, maxLength) + '...';
+    }
+    return description;
 }
