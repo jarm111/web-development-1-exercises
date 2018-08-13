38b0e5d93464a9e9feeb401d7b1201c90ed6d9c4
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2018-08-13 14:09:39 +0300

    Books output to table

--- exercise16-web-api/index.js
+++ exercise16-web-api/index.js
@@ -1,9 +1,28 @@
+
 $(document).ready(function () {
     $.getJSON('https://www.googleapis.com/books/v1/volumes?q=javascript', function (data) {
         for (var i = 0; i < data.items.length; i++) {
-            var item = data.items[i];
-            $('#content').append('<br>' + item.volumeInfo.title);
+            // var item = data.items[i];
+            // $('#content').append('<br>' + item.volumeInfo.title);
+            var vol = data.items[i].volumeInfo;
+            appendBooks(i, vol.title, vol.authors[0], vol.publishedDate, vol.description);
         }
         console.log(data);
     });
 });
+
+function appendBooks(tableRowIndex, title, author, date, description) {
+    $('#books').append(
+        '<tr><th scope="row">'
+        + ++tableRowIndex +
+        '</th><td>'
+         + title +
+         '</td><td>'
+         + author +
+         '</td><td>'
+         + date +
+         '</td><td>'
+         + description +
+         '</td></tr>'
+    );
+}
