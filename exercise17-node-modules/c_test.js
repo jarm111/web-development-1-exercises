c6314f79f7facf88dff0f1432a0f51708abc8ad1
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2018-08-14 14:31:53 +0300

    Messing with spread syntax and map()

--- exercise17-node-modules/c_test.js
+++ exercise17-node-modules/c_test.js
@@ -1,8 +1,14 @@
-let myObj = require('./c_module');
+const myObj = require('./c_module');
 
-let random = myObj.randomNumber(50, 99);
-console.log(random);
+// const minMaxes = [[0, 1], [1, 10], [10, 100], [999, 9999], [-5, 0]];
+// for (let minMax of minMaxes) {
+//     let result = myObj.randomNumber(...minMax);
+//     console.log(result);
+// }
 
-let numbers = [1, 20, 9, 11];
-let average = myObj.calcAverage(numbers);
+[[0, 1], [1, 10], [10, 100], [999, 9999], [-5, 0]]
+    .map(a => console.log(myObj.randomNumber(...a)));
+
+const numbers = [1, 20, 9, 11];
+const average = myObj.calcAverage(numbers);
 console.log(average);
