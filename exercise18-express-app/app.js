af9efe8f70186c74928e4b0ee548b2f542b9c294
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2018-08-15 10:58:16 +0300

    Starting point for the exercise

--- exercise18-express-app/app.js
+++ exercise18-express-app/app.js
@@ -4,6 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var session = require('express-session');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -19,23 +21,33 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+/*Session käyttöönotto
+  Sessio toimii siten että luotaessa sessio syntyy selaimelle automaattisesti cookie jonka
+  nimi on connect.sid (sessionid). Se ylläpitää yhteyttä palvelimella olevaan sessioon
+  sen sisältämän sessioid:n avulla.
+  Itse sessio sisältää sessiomuuttujia, joita voidaan lukea siirryttäessä sivulta toiselle.
+  Jos sessiomuuttujana on vaikka salasana, niin sivuille siirryttäessä voidaan tutkia onko
+  salasana oikea ja jos on, päästetään käyttäjä sivulle.
+*/
+app.use(session({ secret: 'salausarvo', cookie: { maxAge: 60000 }, resave: true, saveUninitialized: true }));
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
--- exercise18-express-app/routes/index.js
+++ exercise18-express-app/routes/index.js
@@ -1,9 +1,67 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+var sess;
+
+/* Kun tullaan juureen tehdään nämä jutut */
+router.get('/', function (req, res, next) {
+    sess = req.session; //laitetaan sessio-olio muuttujaan sess
+
+    if (sess.pass === 'qwerty') {
+        res.redirect('/salasivu1'); //salainen sivu
+    } else {
+        res.render('index', {
+            title: 'Express sessioesimerkki'
+        }); //kirjautumissivu
+    }
+});
+
+//reitti salasivu1-sivulle
+router.get('/salasivu1', function (req, res) {
+    sess = req.session; //laitetaan sessio-olio muuttujaan sess
+    //sivu on suojattu salasanalla. Sen voisi hakea ulkoisesta tiedostosta tai kannasta
+    if (sess.pass === 'qwerty') {
+        res.render('salasivu1', {
+            title: 'Olet nyt sessiossa sivulla salasivu1!',
+            sessid: sess.id
+        });
+    }
+    else {
+        res.render('error', {// jos passu väärä, mennään error-sivulle
+            message: 'Et ole kirjautunut tai salasanasi on väärä',
+        });
+    }
+});
+
+//reitti salasivu2-sivulle
+router.get('/salasivu2', function (req, res) {
+    sess = req.session; //laitetaan sessio-olio muuttujaan sess
+    //tätä sivua ei ole suojattu salasanalla, mutta sessiossa ollaan silti
+    res.render('salasivu2', {
+        title: 'Olet nyt sessiossa sivulla salasivu2!',
+        sessid: sess.id
+    }); //salainen sivu
+});
+
+router.get('/logout', function (req, res) {
+    req.session.destroy(function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.redirect('/');//takaisin juureen eli kirjautumaan
+        }
+    });
+});
+
+/*
+login -reitti on post-tyyppinen eli se vastaanottaa
+post-metodilla lähetettyä dataa
+*/
+router.post('/login', function (req, res) {
+    sess = req.session;
+    sess.email = req.body.email; //sess.email saa arvon login-sivulta (index.ejs)
+    sess.pass = req.body.pass;////sess.pass saa arvon login-sivulta (index.ejs)
+    res.end('done'); //response eli vastaus on 'done' ja se saadaan login-sivulle
 });
 
 module.exports = router;
