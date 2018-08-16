3f42987c55a3456867d3881dc2bc139f19e0211d
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2018-08-16 15:25:15 +0300

    Moved api routes to Tasks

---
+++ exercise19-restful-api-tutorial/routes/Tasks.js
@@ -0,0 +1,56 @@
+var express = require('express');
+var router = express.Router();
+var Task = require('../models/Task');
+
+router.get('/:id?', function(req, res, next) {
+    if (req.params.id) {
+        Task.getTaskById(req.params.id, function(err, rows) {
+            if (err) {
+                res.json(err);
+            } else {
+                res.json(rows);
+            }
+        });
+    } else {
+        Task.getAllTasks(function(err, rows) {
+            if (err) {
+                res.json(err);
+            } else {
+                res.json(rows);
+            }
+        });
+    }
+    next();
+});
+
+router.post('/', function(req, res, next) {
+    Task.addTask(req.body, function(err, count) {
+        if (err) {
+            res.json(err);
+        } else {
+            res.json(req.body);
+        }
+    });
+
+    Task.deleteTask(req.params.id, function(err, count) {
+        if (err) {
+            res.json(err);
+        } else {
+            res.json(count);
+        }
+    });
+    next();
+});
+
+router.put('/:id', function(req, res, next) {
+    Task.updateTask(req.params.id, req.body, function(err, rows) {
+        if (err) {
+            res.json(err);
+        } else {
+            res.json(rows);
+        }
+    });
+    next();
+});
+
+module.exports = router;
