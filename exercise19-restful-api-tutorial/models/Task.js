49e6f1f4bd45103bce1507159db58fe5f7ee5845
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2018-08-16 14:31:18 +0300

    Created Task

---
+++ exercise19-restful-api-tutorial/models/Task.js
@@ -0,0 +1,25 @@
+var db = require('../dbconnection');
+
+var Task = {
+    getAllTasks: function(callback) {
+        return db.query('SELECT * FROM tasks', callback);
+    },
+
+    getTaskById: function(id, callback) {
+        return db.query('SELECT * FROM task WHERE Id=?', [id], callback);
+    },
+
+    addTask: function(Task, callback) {
+        return db.query('INSERT INTO task VALUES(?, ?, ?)',[Task.Id, Task.Title, Task.Status], callback);
+    },
+
+    deleteTask: function(id, callback) {
+        return db.query('DELETE FROM task WHERE Id=?', [id], callback);
+    },
+
+    updateTask: function(id, Task, callback) {
+        return db.query('UPDATE task SET Title=?, Status=? WHERE Id=?', [Task.Title, Task.Status, id], callback);
+    }
+};
+
+module.exports = Task;
