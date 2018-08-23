cba298df1a1b6e730e337b7e6bf8877256c81af8
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2018-08-23 15:32:28 +0300

    Added comments

--- exercise21-rest-jwt-auth/config.js
+++ exercise21-rest-jwt-auth/config.js
@@ -1,3 +1,4 @@
+// secret key for JSON Web Token, public for demo purposes
 module.exports = {
     'secret': 'supersecret'
 };
--- exercise21-rest-jwt-auth/routes/auth.js
+++ exercise21-rest-jwt-auth/routes/auth.js
@@ -1,3 +1,8 @@
+/*
+ * Created with a help from an example found:
+ * https://medium.freecodecamp.org/securing-node-js-restful-apis-with-json-web-tokens-9f811a92bb52 
+ */
+
 var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
@@ -11,6 +16,7 @@ var tokenExpireTime = 600; // 10 minutes
 
 // Register new user
 router.post('/register', function (req, res) {
+    // encrypt password
     var hashedPassword = bcrypt.hashSync(req.body.password, 8);
 
     User.addUser(
@@ -23,6 +29,8 @@ router.post('/register', function (req, res) {
             if (err) return res.status(500).send('There was a problem registering the user');
             
             // create a token, user.insertId is the auto-increment id
+            // the jwt.sign() method takes a payload and the secret key defined in config.js as parameters.
+            // It creates a unique string of characters representing the payload, this case the id of the user
             var token = jwt.sign({id: user.insertId}, config.secret, {
                 expiresIn: tokenExpireTime
             });
@@ -33,13 +41,16 @@ router.post('/register', function (req, res) {
 });
 
 // Get logged in user info
+// expect the token be sent along with the request in the headers
 router.get('/me', function (req, res) {
+    // The default name for a token in the headers of an HTTP request is x-access-token
     var token = req.headers['x-access-token'];
 
     if (!token) {
         return res.status(401).send({ auth: false, message: 'No token provided' });
     }
 
+    // decodes the token making it possible to view the original payload
     jwt.verify(token, config.secret, function (err, decoded) {
         if (err) {
             return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
@@ -51,6 +62,7 @@ router.get('/me', function (req, res) {
             if (!user) return res.status(404).send('No user found');
             // don't show password
             user[0].password = 0;
+            // send back the user object
             res.status(200).send(user);
         });
     });
@@ -61,10 +73,12 @@ router.post('/login', function(req, res) {
     User.getUserByEmail(req.body.email, function(err, user) {
         if (err) return res.status(500).send('Error on the server');
         if (!user[0]) return res.status(404).send('No user found');
-
+        
+        // compare the password sent with the request to the password in the database 
         var passwordIsValid = bcrypt.compareSync(req.body.password, user[0].password);
         if (!passwordIsValid) return res.status(401).send({auth: false, token: null});
 
+        // sign a token
         var token = jwt.sign({id: user.insertId}, config.secret, {
             expiresIn: tokenExpireTime
         });
@@ -73,6 +87,7 @@ router.post('/login', function(req, res) {
     });
 });
 
+// logout user, usually done client side by destroying the token
 router.get('/logout', function(req, res) {
     res.status(200).send({auth: false, token: null});
 });
